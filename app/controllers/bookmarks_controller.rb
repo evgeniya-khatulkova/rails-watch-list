f922a374727698cdb4637cc2ea948bbf730f48f9
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-02-24 15:09:31 +0100

    delete and create bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,6 +13,12 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path, status: :see_other
+  end
+
 private
 
   def bookmark_params
